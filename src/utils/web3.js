b0322c60d0d8f025b93115faa1e78b76ba183a65
Mashood Ahmed <famashood@gmail.com>
2024-01-19 16:54:38 +0500

    blockchain integration

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -1,183 +1,28 @@
-import Web3 from "web3"
+import { Wallet, ethers, Contract } from "ethers";
+import dotenv from "dotenv";
+dotenv.config();
 
-// Configure web3 to connect to the Sepolia testnet
-const web3 = new Web3('https://eth-sepolia.g.alchemy.com/v2/0PehriR-bczOTDONtVmXdZmZP4cXn7mt'); // Replace with the actual Sepolia testnet URL
+import abi from "../web3/contract_abi.js"
 
-// ABI and contract address of your Ethereum smart contract
-const contractABI = [
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "payer",
-          "type": "address"
-        },
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "amount",
-          "type": "uint256"
-        },
-        {
-          "indexed": false,
-          "internalType": "string",
-          "name": "description",
-          "type": "string"
-        },
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "timestamp",
-          "type": "uint256"
-        },
-        {
-          "indexed": false,
-          "internalType": "string",
-          "name": "userId",
-          "type": "string"
-        },
-        {
-          "indexed": false,
-          "internalType": "string",
-          "name": "transactionId",
-          "type": "string"
-        }
-      ],
-      "name": "PaymentRecorded",
-      "type": "event"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "_transactionId",
-          "type": "string"
-        }
-      ],
-      "name": "getPaymentDetails",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "payer",
-          "type": "address"
-        },
-        {
-          "internalType": "uint256",
-          "name": "amount",
-          "type": "uint256"
-        },
-        {
-          "internalType": "string",
-          "name": "description",
-          "type": "string"
-        },
-        {
-          "internalType": "uint256",
-          "name": "timestamp",
-          "type": "uint256"
-        },
-        {
-          "internalType": "string",
-          "name": "userId",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "name": "payments",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "payer",
-          "type": "address"
-        },
-        {
-          "internalType": "uint256",
-          "name": "amount",
-          "type": "uint256"
-        },
-        {
-          "internalType": "string",
-          "name": "description",
-          "type": "string"
-        },
-        {
-          "internalType": "uint256",
-          "name": "timestamp",
-          "type": "uint256"
-        },
-        {
-          "internalType": "string",
-          "name": "userId",
-          "type": "string"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "_payer",
-          "type": "address"
-        },
-        {
-          "internalType": "uint256",
-          "name": "_amount",
-          "type": "uint256"
-        },
-        {
-          "internalType": "string",
-          "name": "_description",
-          "type": "string"
-        },
-        {
-          "internalType": "string",
-          "name": "_transactionId",
-          "type": "string"
-        },
-        {
-          "internalType": "string",
-          "name": "_userId",
-          "type": "string"
-        }
-      ],
-      "name": "recordPayment",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    }
-  ];
+const { WEB3_KEY } = process.env;
 
-const contractAddress = '0xA770A5d43E169e5f52f0308615F4BA078aD6Ab3d';
+async function recordPayment(amount, task_id, transaction_id, user_id) {
+    const provider = new ethers.WebSocketProvider('wss://eth-sepolia.g.alchemy.com/v2/0PehriR-bczOTDONtVmXdZmZP4cXn7mt');
+    
+    const contractAddress = "0xa4dc3E9f536E254bf0b63df536C3a60572297660";
+    
+    let wallet = new Wallet(WEB3_KEY, provider);
 
-// Create a contract instance
-const contract = new web3.eth.Contract(contractABI, contractAddress);
+    const paymentContract = new Contract(contractAddress, abi, wallet);
 
-const recordPayment = async (payer,amount,description,transactionId) => {
-    const result = await contract.methods.recordPayment(
-        payer,
-        amount,
-        description,
-        transactionId
-      ).send({
-        from: '0xb205B60aae2361d479CE6EF4b33acEbD50748819', // Replace with your wallet address
-        gas: 2000000 // Adjust gas limit as needed
-      });
+    const tx = await paymentContract.recordPayment("0x1234567890123456789012345678901234567890", amount, task_id, transaction_id, user_id);
 
-      return result
-}
+    const txReceipt = await tx.wait();
+
+    // Transaction mined successfully
+    return txReceipt.transactionHash;
+
+
+   }
 
 export default recordPayment
---
+++ src/web3/contract_abi.js
@@ -0,0 +1,158 @@
+const abi = [
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "payer",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "description",
+          "type": "string"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "timestamp",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "userId",
+          "type": "string"
+        },
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "transactionId",
+          "type": "string"
+        }
+      ],
+      "name": "PaymentRecorded",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_transactionId",
+          "type": "string"
+        }
+      ],
+      "name": "getPaymentDetails",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "payer",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "description",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "timestamp",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "userId",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "name": "payments",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "payer",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "description",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "timestamp",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "userId",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_payer",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_amount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "_description",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_transactionId",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_userId",
+          "type": "string"
+        }
+      ],
+      "name": "recordPayment",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
+
+export default abi
